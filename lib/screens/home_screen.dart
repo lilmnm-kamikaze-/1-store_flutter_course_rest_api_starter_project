dc61dce849e767ff661ea193baa48312d41bc948
kami <lilmnm-kamikaze-@users.noreply.github.com>
2023-07-31 02:31:09 -0400

    App bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_api_flutter_course/consts/global_colors.dart';
+
+import '../widget/appbar_icons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +12,75 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
-      ),
+      child: Scaffold(
+          appBar: AppBar(
+            // elevation: 4,
+            title: const Text('Home'),
+            leading: AppBarIcons(
+              function: () {},
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(
+                function: () {},
+                icon: IconlyBold.user3,
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 18,
+                ),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).cardColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      suffixIcon: Icon(
+                        IconlyLight.search,
+                        color: lightIconsColor,
+                      )),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
